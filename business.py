9543515aceb2e7f0c945fd85a47d0184b2c45115
vdgutierrez <116768443+vdgutierrez@users.noreply.github.com>
2024-09-07 18:35:52 +0000

    vlad_register

--- business.py
+++ business.py
@@ -1,4 +1,15 @@
-from db import db_connection
+import mysql.connector as mysql
+
+def db_connection():
+    conexion = mysql.connect(
+        host="sql10.freemysqlhosting.net",
+        user="sql10729427",
+        password="ji7RdGLaZq",
+        database="sql10729427",
+        port=3306
+    )
+    print(conexion)
+    return conexion
 
 def read_data():
     connetion = db_connection()
@@ -7,9 +18,39 @@ def read_data():
     result = cursor.fetchall()
     for row in result:
         print(row)
-    
     cursor.close()
     connetion.close()
 
-read_data()
+def insert_perfil(name, lastname, email, password, tipo):
+    connetion = db_connection()
+    cursor = connetion.cursor()
+    usuario = name + ' ' + lastname
+    cursor.execute("INSERT INTO PERFIL (USUARIO,CONTRASENA, CORREO, TIPO) VALUES (%s, %s, %s, %s)", (usuario, password, email, tipo))
+    connetion.commit()
+    cursor.close()
+    connetion.close()
+
+def insert_asistente(name, lastname):
+    connetion = db_connection()
+    cursor = connetion.cursor()
+    cursor.execute("INSERT INTO ASISTENTE (NOMBRES, APELLIDOS) VALUES (%s, %s)", (name, lastname))
+    connetion.commit()
+    cursor.close()
+    connetion.close()
+
+def insert_organizador(name, lastname):
+    connetion = db_connection()
+    cursor = connetion.cursor()
+    cursor.execute("INSERT INTO ORGANIZADOR (NOMBRES, APELLIDOS) VALUES (%s, %s)", (name, lastname))
+    connetion.commit()
+    cursor.close()
+    connetion.close()
+
+def insert_orador(name, lastname):
+    connetion = db_connection()
+    cursor = connetion.cursor()
+    cursor.execute("INSERT INTO ORADOR (NOMBRES, APELLIDOS) VALUES (%s, %s)", (name, lastname))
+    connetion.commit()
+    cursor.close()
+    connetion.close()
 
--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 from flask import Flask, render_template, request
+from business import *
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('/landing/index.html')
+    return render_template('index.html')
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
+    print(request.form)
+    if request.method == 'POST':
+        name = request.form['name']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        password = request.form['password']
+        tipo = request.form['tipo']
+        print(name)
+        print(lastname)
+        print(email)
+        print(password)
+        print(tipo)
+        insert_perfil(name, lastname, email, password, tipo)
+        if tipo == 'Asistente':
+            insert_asistente(name, lastname)
+        if tipo == 'Organizador':
+            insert_organizador(name, lastname)
+        
+        if tipo == 'Orador':
+            insert_orador(name, lastname)
+
     return render_template('register.html')
 
-@app.route('/question')
-def question():
-    return render_template('question.html')
+@app.route('/metting')
+def metting():
+    return render_template('metting.html')
 
-@app.route('/login_user', methods=['POST'])
+@app.route('/login_user', methods=['GET','POST'])
 def login_user():
     if request.method == 'POST':
         username = request.form['username']
